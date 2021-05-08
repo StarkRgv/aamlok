9f37ddcd47c59287ef567a72cbb13a2655b4d548
Santhosh Ragavu <ragavudev@gmail.com>
2021-05-08 12:28:24 +0530

    May 8

--- app/Http/Controllers/DetailsController.php
+++ app/Http/Controllers/DetailsController.php
@@ -5,17 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\ProductVariation;
+use App\Models\ProductVariationImages;
+
 
 
 class DetailsController extends Controller
 {
     public function openDetails(Request $request, Product $product){
+        // return $request->color;
         $stockCount = ProductVariation::where('product_id', $product->id)->where('status', 1)->sum('quantity');
         $sizes = ProductVariation::where('product_id', $product->id)->where('status', 1)->distinct()->get(['size']);
-        // return $sizes;
+        $colors = ProductVariation::where('product_id', $product->id)->where('status', 1)->distinct()->get(['color']);
+        $imageId = ProductVariationImages::where('product_id', $product->id)->distinct()->first();
+
+        if ($request->has('color')) {
+            $images = ProductVariationImages::where('product_id', $product->id)->where('color_id', $request->color)->get();
+        }else{
+            $images = ProductVariationImages::where('product_id', $product->id)->where('color_id', $imageId->color_id)->get();
+        }
+
     	return view('detail', ['product' => $product,
                                'stockCount' => $stockCount,
-                               'sizes' => $sizes
+                               'sizes' => $sizes,
+                               'colors' => $colors,
+                               'images' => $images
                                ]);
     }
 }
